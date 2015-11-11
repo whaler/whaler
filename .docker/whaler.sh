2304ccaf7f467a1a6f56f9436c6f9a607c60c5f0
cravler <sergeizz@gmail.com>
2015-11-11 13:50:25 +0200

    WHALER_FRONTEND

--- .docker/whaler.sh
+++ .docker/whaler.sh
@@ -3,16 +3,23 @@
 set -e
 
 : ${WHALER_PATH:=}; export WHALER_PATH
+: ${WHALER_FRONTEND:=}; export WHALER_FRONTEND
 
 if [ ! -z "$WHALER_PATH" ]; then
     WHALER_VOLUME="-v $WHALER_PATH:/usr/local/lib/node_modules/whaler"
 fi
 
-docker run -it --rm \
+DOCKER_OPTS="-t"
+if [ -z "$WHALER_FRONTEND" ]; then
+    DOCKER_OPTS="-it"
+fi
+
+docker run $DOCKER_OPTS --rm \
 -v $HOME:$HOME \
 -v $HOME/.whaler:/root/.whaler \
 $WHALER_VOLUME \
 -w `pwd` \
+-e "WHALER_FRONTEND=$WHALER_FRONTEND" \
 --pid host \
 --volumes-from whaler \
 --name whaler_$$ \
