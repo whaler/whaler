bdc4bedfd5836a531365239cee04886096e505ba
cravler <sergeizz@gmail.com>
2020-10-19 12:02:00 +0300

    docker API v1.40

--- src/docker.js
+++ src/docker.js
@@ -10,6 +10,6 @@ if (!stats.isSocket()) {
 }
 
 module.exports = new Docker({
-    version: process.env.WHALER_DOCKER_API || 'v1.26',
+    version: process.env.WHALER_DOCKER_API || 'v1.40',
     socketPath: socket
 });
