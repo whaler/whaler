e67b0d1f98222cc0e04950e193e860e0f52be75c
cravler <sergeizz@gmail.com>
2022-01-23 15:36:00 +0200

    setup script improvements

--- .docker/setup.sh
+++ .docker/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+set -e
+
 OS=linux
+ARCH=amd64
 VERSION=latest
 DIR=/usr/local/bin
 FILE=whaler
+
+SETUP_SCRIPT=https://raw.githubusercontent.com/whaler/whaler/master/.docker/setup.sh
+DOWNLOAD_BUCKET=https://github.com/whaler/whaler-client/releases/download/0.x
 
 for i in "$@"; do
 case $i in
@@ -25,7 +31,7 @@ case $i in
 esac
 done
 
-case "$OSTYPE" in
+case "${OSTYPE}" in
     darwin*)
         OS=darwin
     ;;
@@ -34,26 +40,45 @@ case "$OSTYPE" in
     ;;
 esac
 
+case "$(uname -m)" in
+    aarch64|arm64)
+        ARCH=arm64
+    ;;
+    armv7*)
+        ARCH=arm7
+    ;;
+    armv6*)
+        ARCH=arm6
+    ;;
+    *)
+        # other os
+    ;;
+esac
+
 setup_client() {
-    mkdir -p $DIR
-    curl -SL -o $DIR/$FILE https://github.com/whaler/whaler-client/releases/download/$1_amd64/whaler
-    if [ -f $DIR/$FILE ]; then
-        chmod 0755 $DIR/$FILE
+    mkdir -p ${DIR}
+    curl -SL ${DOWNLOAD_BUCKET}/whaler_${OS}_${ARCH}.tar.gz | tar -xz -C ${DIR} whaler
+    if [ -f ${DIR}/whaler ]; then
+        chmod 0755 ${DIR}/whaler
+
+        if [ "${DIR}/whaler" != "${DIR}/${FILE}" ]; then
+            mv ${DIR}/whaler ${DIR}/${FILE}
+        fi
     fi
 }
 
 setup_whaler() {
-    $DIR/$FILE setup --version $1
+    ${DIR}/${FILE} setup --version ${1}
 }
 
-if [ ! -z "$SUDO_USER" ]; then
-    setup_client $OS
+if [ ! -z "${SUDO_USER}" ]; then
+    setup_client
 else
-    if [ -w $DIR ]; then
-        setup_client $OS
-        setup_whaler $VERSION
+    if [ -w ${DIR} ]; then
+        setup_client
+        setup_whaler ${VERSION}
     else
-        curl -sSL https://raw.githubusercontent.com/whaler/whaler/master/.docker/setup.sh | sudo sh -s -- --version="$VERSION" --dir="$DIR" --file="$FILE"
-        setup_whaler $VERSION
+        curl -sSL ${SETUP_SCRIPT} | sudo sh -s -- --version="${VERSION}" --dir="${DIR}" --file="${FILE}"
+        setup_whaler ${VERSION}
     fi
 fi
